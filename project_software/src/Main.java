60a5a6a34215b77dd9566e25f38c3b13da9ef5fd
Nikolay Mutafchiev <niki_mutafchiev@yahoo.com>
2024-01-04 12:36:53 +0200

    Creating UserInterface

--- project_software/src/Main.java
+++ project_software/src/Main.java
@@ -1,5 +1,8 @@
+import application.UserInterface;
+
 public class Main {
     public static void main(String[] args){
-        
+        UserInterface console = UserInterface.getSingleton_instance();
+
     }
 }
---
+++ project_software/src/application/UserInterface.java
@@ -0,0 +1,45 @@
+package application;
+
+public class UserInterface {
+    private int num_games;
+    private int num_players;
+    private int num_teams;
+    private static UserInterface singleton_instance = null;
+
+    private UserInterface(){
+        num_games = 0;
+        num_players = 0;
+        num_teams = 0;
+    }
+
+    public static UserInterface getSingleton_instance(){
+        if(singleton_instance == null)
+            singleton_instance = new UserInterface();
+        return singleton_instance;
+    }
+    public int startGame(int team1id, int team2id){
+        if(team1id< 0 || team1id>num_teams || team2id< 0 || team2id > num_teams)
+            return -1; // will throw exception
+        int generated_id = 0; //will return the id in the database
+        return generated_id;
+    }
+    public boolean endGame(int gameId, int score){
+        if(score < 0 ||  gameId < 0 || gameId > num_games ){
+            return false; // will throw exception
+        }
+        return true;
+    }
+    public int addTeam(String name, int[] players){
+        int generated_id = 0; //will return the id in the database
+        return generated_id;
+    }
+    public int addPlayer(String name, int jerseyNumber){
+        int generated_id = 0; //will return the id in the database
+        return generated_id;
+    }
+    public void addPlayerToTeam(int playerId, int teamId){
+        if(playerId< 0 || playerId > num_players || teamId< 0 || teamId > num_teams)
+            return; // will throw exception
+
+    }
+}
