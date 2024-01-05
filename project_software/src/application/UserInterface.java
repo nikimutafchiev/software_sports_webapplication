5069c148270604580386f29dc9074b2ea44c7f3c
Nikolay Mutafchiev <niki_mutafchiev@yahoo.com>
2024-01-05 18:00:25 +0200

    Adding exceptions

--- project_software/src/application/UserInterface.java
+++ project_software/src/application/UserInterface.java
@@ -1,5 +1,9 @@
 package application;
-
+class InvalidParameterException extends Exception{
+    InvalidParameterException(String message){
+        super(message);
+    }
+}
 public class UserInterface {
     private int num_games;
     private int num_players;
@@ -17,29 +21,35 @@ public class UserInterface {
             singleton_instance = new UserInterface();
         return singleton_instance;
     }
-    public int startGame(int team1id, int team2id){
+    public int startGame(int team1id, int team2id) throws InvalidParameterException{
         if(team1id< 0 || team1id>num_teams || team2id< 0 || team2id > num_teams)
-            return -1; // will throw exception
+            throw new InvalidParameterException("Invalid team id");
+        //TODO
         int generated_id = 0; //will return the id in the database
         return generated_id;
     }
-    public boolean endGame(int gameId, int score){
-        if(score < 0 ||  gameId < 0 || gameId > num_games ){
-            return false; // will throw exception
-        }
-        return true;
+    public void endGame(int gameId, int score) throws InvalidParameterException{
+        if(score < 0 )
+            throw new InvalidParameterException("Invalid score");
+        if(gameId < 0 || gameId > num_games)
+            throw new InvalidParameterException("Invalid game id");
+        //TODO
     }
     public int addTeam(String name, int[] players){
+        //TODO
         int generated_id = 0; //will return the id in the database
         return generated_id;
     }
     public int addPlayer(String name, int jerseyNumber){
+        //TODO
         int generated_id = 0; //will return the id in the database
         return generated_id;
     }
-    public void addPlayerToTeam(int playerId, int teamId){
-        if(playerId< 0 || playerId > num_players || teamId< 0 || teamId > num_teams)
-            return; // will throw exception
-
+    public void addPlayerToTeam(int playerId, int teamId) throws InvalidParameterException{
+        if(playerId< 0 || playerId > num_players)
+            throw new InvalidParameterException("Invalid player id");
+        if (teamId< 0 || teamId > num_teams)
+            throw new InvalidParameterException("Invalid team id");
+        //TODO
     }
 }
